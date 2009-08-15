c1c7eb702b8435427993b76c6d0d63795cadf598
Sameer Rahmani <lxsameer@gmail.com>
2009-08-15 12:32:36 +0400

    Add some documents

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -14,4 +15,15 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # (r'^admin/', include(admin.site.urls)),
+)
+
+
+
+# this section add site media to the development environment in release time we should use apache for handling this
+if settings.DEBUG :
+    
+    urlpatterns += patterns ('' ,
+       (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',  {'document_root':os.path.join ( os.path.dirname (__file__) , 'media').replace ('\\' ,'/')}),
+                                  
+                                                  
 )
