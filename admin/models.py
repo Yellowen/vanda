8ef6f5ecc7be42faaae507242cc0d3d063c9ad0a
Sameer Rahmani <lxsameer@gmail.com>
2009-08-21 14:11:21 +0400

    installer: template model added

--- admin/models.py
+++ admin/models.py
@@ -43,21 +43,6 @@ class application (models.Model):
     def delete (self):
 
         shutil.rmtree (settings.FS_ROOT + '/apps/' + str (self.Name))
-        #print settings.FS_ROOT + '/apps/' + str(self.Name)
-        #self.Public = False
-        #papp = self.objects.filter (Publish = True)
-        #iapp = ()
-    
-        #for i in papp:
-        #    iapp += ('apps.' + str (i.Name) , )
-        #settings.INSTALLED_APPS += iapp
-        #print "dsdsd"
-        
-        #print iapp
-        #os.unlink (settings.FS_ROOT + "/confs/__init__.py")
-        #fd = open (settings.FS_ROOT + "/confs/__init__.py" , 'w')
-        #fd.write ('published_apps = ' + str (iapp).replace (',' , ' , \n'))
-        #fd.close ()
         super (application , self).delete ()
         
 
@@ -69,3 +54,35 @@ class application (models.Model):
         
 
 #+++ maybe its a good idea to add a application category
+
+
+class template (models.Model):
+    
+    Name = models.CharField (max_length = 50,  unique = True , verbose_name = _('Name') )
+   
+    Version = models.CharField (max_length = 15 , verbose_name = _('Version'))
+
+    SHA1 = models.CharField (max_length = 40 , unique = True ,editable = False
+                             , verbose_name = _('SHA1 hash') , 
+                             help_text = _("Each application identify it self by its SHA1 hash"))
+
+    Author = models.CharField (max_length = 30 , verbose_name = _('Author'))
+    Email = models.EmailField (verbose_name = _('Email'))
+    Home = models.URLField (blank = True , verbose_name = _('Home Page') )
+
+    Description = models.TextField (blank = True , verbose_name = _('Description')  )
+
+    Default = models.BooleanField (unique = True , verbose_name = _('Default'))
+
+
+    def delete (self):
+        #+++ here i should add a peace of code that run a hook inside of template directory for returning 
+        #+++ the template dir address
+        pass
+
+
+    def __unicode__ (self):
+        return self.Name
+
+    class Meta :
+        verbose_name_plural = _('Templates')
