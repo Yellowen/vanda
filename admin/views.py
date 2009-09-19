07d8f0885b3d79ca753c9fe5dced7e640b5cc52f
Sameer Rahmani <lxsameer@gmail.com>
2009-09-20 02:24:09 +0400

    DPM page added

--- admin/views.py
+++ admin/views.py
@@ -1,1 +1,12 @@
+from django.shortcuts import render_to_response as rr
 # Create your views here.
+
+
+
+def pkgm_mng (request):
+    return rr ('admin/dpm.html')
+
+
+
+
+
--- urls.py
+++ urls.py
@@ -18,6 +18,7 @@ urlpatterns = patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
+    (r'^admin/dpm/', include('admin.urls')),
     (r'^admin/', include(admin.site.urls)),
 )
 
