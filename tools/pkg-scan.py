d95a4fe09b61dea7c07c388fdd1ce09113cfffba
Sameer Rahmani <lxsameer@gmail.com>
2009-09-24 00:36:48 +0300

    pkg-scan.py bug fixed , better out put provided

--- tools/pkg-scan.py
+++ tools/pkg-scan.py
@@ -7,10 +7,12 @@ import simplejson as json
 def rese (path , js):
     lst = os.listdir (path)
     lst2 = list ()
-    
+    ret = list ()
     for i in lst:
-        if os.path.isdir (i):
-            lst2 = lst2[:] + rese (path + "/" + i , js)[:]
+        
+        if os.path.isdir (os.path.join (os.path.dirname (path + "/"  + i) , i).replace ('\\' , '/')):
+            
+            lst2 = lst2[:] + rese (path + "/" + i + "/", js)[:]
         else:
             di = dict () 
             name = i.split("-")[0]
@@ -20,10 +22,12 @@ def rese (path , js):
             m = hashlib.sha1 ()
             m.update (file (path + "/" + i , 'r').read())
             di["SHA1"] = m.hexdigest ()
-            di["Address"] = os.path.join (os.path.dirname (i) , i).replace ('\\' , '/')
+            di["Address"] = os.path.join (os.path.dirname (path + "/" + i) , i).replace ('\\' , '/')
+            
             lst2.append (di)
-    js = js[:] + lst2[:]
-    return js
+            
+    ret = js[:] + lst2[:]
+    return ret 
 
 
 
@@ -40,8 +44,12 @@ if url == "help":
     print "$ pkg-scan.py /home/pool/main\n"
     sys.exit(0)
 
-a = rese (url)
-print a
+if url[-1] == "/":
+    url = url[:-1]
+
+b = list ()
+a = rese (url , b)
+print json.JSONEncoder().encode (a).replace (',' , ',\n').replace ('},' , '},\n').replace ('"}' , '"\n}').replace ('{"' , '{\n"')
 
 
 
