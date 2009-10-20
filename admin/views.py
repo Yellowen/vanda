0002cb18604a26635420946aa1e78a5127853e34
Sameer Rahmani <lxsameer@gmail.com>
2009-10-20 23:19:11 +0300

    Basic view of DPM interface completed.

--- admin/views.py
+++ admin/views.py
@@ -20,13 +20,17 @@ def installer_test (request ):
     dpm.update ()
     pkg1 = dpm.pkglist ()
     pkg = list ()
+    a = 1
     for i in  pkg1:
-        m = i.split ("::")[1]
+        m = i.split ("::")[5]
         if m == "*":
             check = "checked"
         else:
             check = ""
-        pkg.append ({"id" : i , "checked" : check , "name" : i.split("::")[0]})
-        
+        if (a % 2) == 0 :
+            pkg.append ({"id" : i , "checked" : check , "name" : i.split("::")[0] , "version" : i.split("::")[2] , "short_desc" :  i.split("::")[4] , "color" : "a"})
+        else:
+            pkg.append ({"id" : i , "checked" : check , "name" : i.split("::")[0] , "version" : i.split("::")[2] , "short_desc" :  i.split("::")[4]})
+        a += 1
     return rr ('admin/dpm/pkglist.html' , {"pkg" : pkg})
     
