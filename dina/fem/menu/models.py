391d95a484aac5b81633632558f8714888447ba4
Sameer Rahmani <lxsameer@gmail.com>
2009-11-27 23:44:59 +0300

    Menu change view phase 0 completed

--- dina/fem/menu/models.py
+++ dina/fem/menu/models.py
@@ -19,7 +19,9 @@ class item (models.Model):
     title = models.CharField (max_length = 30 , verbose_name = _("Title"))
     url = models.CharField (max_length = 100 , verbose_name = _("Target"))
     #newwindow
+
     weight = models.IntegerField (max_length = 2 , verbose_name = _("Weight") , default = 0 , help_text = _("A menu with the lower weight value will stay in top. (Not in Shamsiel WebDesk.)"))
+    publish = models.BooleanField (default = False , verbose_name = _("Target"))
     def __unicode__ (self):
         return self.title
 
--- dina/fem/menu/views.py
+++ dina/fem/menu/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render_to_response as rr
+from django.template import Context , Template
 from django.contrib.auth.decorators import user_passes_test
 from models import *
 # Create your views here.
+
+
+
+def gentree (x):
+    tree = "<li><a href='/admin/menu/menu/%d/'>%s</a><ul>\n" % (x.id , x)
+    for i in x.get_children ():
+        tree = tree +  gentree (i)
+    
+    for i in x.items.filter (publish = True):
+        tree = tree + "<li><a href='/admin/menu/item/%d/'>%s</a></li>\n" % (i.id , i)
+    tree = tree + "</ul></li>\n"
+    return tree
+    
 
 
 #+++ TODO: add permission checker for menu editing
 @user_passes_test(lambda u: u.is_superuser , login_url='/admin/')
 def change_list (request):
     root_menus = menu.objects.filter (parent = None)
-    return rr ('admin/change_tree.html')
+    tree = "<ul>\n"
+    for i in root_menus:
+        tree = tree + gentree (i)
+    tree = tree + "</ul>\n"
+    t = Template (tree)
+    return rr ('admin/change_tree.html', {"msg" : "Menu View" , "tree" : t.render (Context ())})
