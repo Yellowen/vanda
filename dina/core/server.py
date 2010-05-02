dfb105e4a898761306024e5b263d6704c21b2085
Nima Nazari <eminem_d12n@yahoo.com>
2010-05-02 17:56:03 +0430

    some bug fixed in server.py

--- dina/core/server.py
+++ dina/core/server.py
@@ -9,13 +9,17 @@ from django.conf import settings
 def MediaServ (request ,  path):
     current = Template.objects.Current ()
     i = 0
-    mtype = 'plain/text'
-    if path[-4:] in ['.jpg' , '.png']:
-        mtype = 'image/*'
+
     # TODO: search in the TEMPLATE_DIRS for the statics files , not in the first element only
     fd = open (settings.TEMPLATE_DIRS[0] + "/" + current + '/media/' + path ,  'r''')
     buf = fd. read ()
     fd.close ()
+    mtype = 'plain/text'
+    if path[-4:] in ['.jpg' , '.png']:
+        mtype = 'image/*'
+    if path[-4:] in ['.css']:
+
+        mtype = 'text/css'
     return HttpResponse (buf , mimetype=mtype)
         
 
