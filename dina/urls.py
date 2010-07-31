1dd5f61b5e415cdd5f9ed6608f689ac5a36baffa
Sameer Rahmani <lxsameer@gmail.com>
2010-07-31 21:21:28 +0430

    idea testing in conf

--- dina/urls.py
+++ dina/urls.py
@@ -41,6 +41,8 @@ urlpatterns = patterns('',
     #  (r'^dpm/', include('dina.core.urls')),
     (r'^core/$', 'dina.core.views.pkgm_mng'),                   
     (r'^menu/menu/$', 'dina.fem.menu.views.change_list'),
+    # regex should change according to django naming policy
+    (r'^([A-Za-z][A-Za-z0-9]{1,30})/config/$', 'dina.conf.views.conf_view'),    
     url(r'^$', wrap(admin_index), name='index'),
     (r'^', include(admin.site.urls)),
                        
