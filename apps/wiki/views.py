02fc38c3f1a139683d045bbb9b39a33d1ec5f119
Behnam Ahmad Khan Beigi <b3hnam@b3hnam.com>
2010-10-31 16:38:54 +0330

    wiki edit_page view added

---
+++ apps/wiki/views.py
@@ -0,0 +1,8 @@
+from models import Page
+from django.shortcuts import render_to_response as rtr
+
+def view_page(request, page_name):
+    try:
+        page = Page.objects.get(pk=page_name)
+    except Page.DoesNotExist:
+        return rtr("create.html",{"page_name":page_name})
