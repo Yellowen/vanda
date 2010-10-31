3e184d6f10675e4c9e1cb32de3fa3593ea2f49de
Behnam Ahmad Khan Beigi <b3hnam@b3hnam.com>
2010-10-31 17:00:24 +0330

    wiki markdown view added

--- apps/wiki/views.py
+++ apps/wiki/views.py
@@ -1,7 +1,7 @@
 from models import Page
 from django.shortcuts import render_to_response as rtr
 from django.http import HttpResponseRedirect
-
+import markdown
 
 def view_page(request, page_name):
     try:
@@ -9,12 +9,12 @@ def view_page(request, page_name):
     except Page.DoesNotExist:
         return rtr("create.html",{"page_name":page_name})
     content = page.content
-    return rtr("view.html",{"page_name":page_name, "content":content})
+    return rtr("view.html",{"page_name":page_name, "content":markdown.markdown(content)})
 
 def edit_page(request, page_name):
     try:
         page = Page.objects.get(pk=page_name)
-        content = page.contents
+        content = page.content
     except Page.DoesNotExist:
         content = ""
     return rtr("edit.html",{"page_name":page_name,"content":content})
