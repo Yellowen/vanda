7bd2c97aa16dc978eea37f5d58fc51562266e71b
Behnam Ahmad Khan Beigi <b3hnam@b3hnam.com>
2010-10-31 16:53:09 +0330

    wiki save_page view created and edit_page view edited

--- apps/wiki/views.py
+++ apps/wiki/views.py
@@ -1,8 +1,31 @@
 from models import Page
 from django.shortcuts import render_to_response as rtr
+from django.http import HttpResponseRedirect
+
 
 def view_page(request, page_name):
     try:
         page = Page.objects.get(pk=page_name)
     except Page.DoesNotExist:
         return rtr("create.html",{"page_name":page_name})
+    content = page.content
+    return rtr("view.html",{"page_name":page_name, "content":content})
+
+def edit_page(request, page_name):
+    try:
+        page = Page.objects.get(pk=page_name)
+        content = page.contents
+    except Page.DoesNotExist:
+        content = ""
+    return rtr("edit.html",{"page_name":page_name,"content":content})
+
+def save_page(request, page_name):
+    content = request.POST["content"]
+    try:
+        page = page.objects.get(pk=page_name)
+        page.content = content
+    except Page.DoesNotExist:
+        page = Page(name=page_name, content=content)
+    page.save()
+    return HttpResponseRedirect("/wiki/" + page_name + "/")
+
