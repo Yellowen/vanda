5d0a119f4c7d664b71bb6b3862824cef0d9dccc6
Lucifer Tenshi <lucifer@scinet.cc>
2010-11-04 12:28:43 +0330

    wiki model cleaned up

--- apps/wiki/models.py
+++ apps/wiki/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
-class Page(models.Model):
-    name = modules.CharField(maxlength="20", primary_key=true)
-    content = models.TextField(blank=True)
- 
 
+class Page(models.Model):
+    name = models.CharField(maxlength="20", primary_key=True)
+    content = models.TextField(blank=True)
     # (r'^Dina/(?P<Page_name>[^/]+/edit/$', 'wikicamp.wiki.views.edit_page'),
