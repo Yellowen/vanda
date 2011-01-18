df1a0d24af000128414fdc29d45c6840b751e35d
Sameer Rahmani <lxsameer@gnu.org>
2011-01-18 12:09:58 +0330

    logging added

---
+++ debbox/core/logging.py
@@ -0,0 +1,62 @@
+# -----------------------------------------------------------------------------
+#    Debbox - Modern administration panel for Debian GNU/Linux
+#    Copyright (C) 2011 Some Hackers In Town
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# -----------------------------------------------------------------------------
+import os
+import logging
+
+from django.conf import settings
+
+logparam = {'filemode' : 'a+'}
+
+try:
+    logparam['level'] = settings.LOG_LEVEL
+except AttributeError:
+    pass
+
+try:
+    logparam['format'] = settings.LOG_FORMAT
+except AttributeError:
+    pass
+
+try:
+    logparam['datefmt'] = settings.LOG_DATE_FORMAT
+except AttributeError:
+    pass
+
+try:
+    if settings.LOG_FILE is not None:
+        if not os.path.exists (settings.LOG_FILE):
+            fd = open (settings.LOG_FILE, 'w')
+            fd.close ()
+        logparam['filename'] = settings.LOG_FILE
+
+except AttributeError:
+    pass
+
+try:
+    logparam['filemode'] = settings.LOG_FILE_MODE
+except AttributeError:
+    pass
+
+
+logging.basicConfig(**logparam)
+
+Logger = logging.getLogger
+
+
+
