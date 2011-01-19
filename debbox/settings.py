5fa74698047c93025ece97931629d02ff7173d07
Sameer Rahmani <lxsameer@gnu.org>
2011-01-19 17:28:27 +0330

    log format documnets added

--- debbox/settings.py
+++ debbox/settings.py
@@ -34,7 +34,31 @@ VERBOSE = 0  # log all levels
 LOG_LEVEL = VERBOSE
 
 # Define the format of log strings
-LOG_FORMAT = '[%(asctime)s] [%(filename)s], \
+#    %(name)s            Name of the logger (logging channel)
+#    %(levelno)s         Numeric logging level for the message (DEBUG, INFO,
+#                        WARNING, ERROR, CRITICAL)
+#    %(levelname)s       Text logging level for the message ("DEBUG", "INFO",
+#                        "WARNING", "ERROR", "CRITICAL")
+#    %(pathname)s        Full pathname of the source file where the logging
+#                        call was issued (if available)
+#    %(filename)s        Filename portion of pathname
+#    %(module)s          Module (name portion of filename)
+#    %(lineno)d          Source line number where the logging call was issued
+#                        (if available)
+#    %(funcName)s        Function name
+#    %(created)f         Time when the LogRecord was created (time.time()
+#                        return value)
+#    %(asctime)s         Textual time when the LogRecord was created
+#    %(msecs)d           Millisecond portion of the creation time
+#    %(relativeCreated)d Time in milliseconds when the LogRecord was created,
+#                        relative to the time the logging module was loaded
+#                        (typically at application startup time)
+#    %(thread)d          Thread ID (if available)
+#    %(threadName)s      Thread name (if available)
+#    %(process)d         Process ID (if available)
+#    %(message)s         The result of record.getMessage(), computed just as
+#                        the record is emitted
+LOG_FORMAT = '[%(asctime)s -  %(funcName)s] [%(filename)s], \
 line:%(lineno)d-> %(levelname)-8s : "%(message)s"'
 
 # Define the date format that use in log strings
