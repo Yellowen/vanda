3d7b8cec577f61610c25b8d740f7b7f53951d679
Sameer Rahmani <lxsameer@gnu.org>
2011-01-22 17:25:30 +0330

    JSONConfParser config parser driver added

---
+++ debbox/core/confparser/parser/JSON.py
@@ -0,0 +1,35 @@
+# -----------------------------------------------------------------------------
+#    Debbox - Modern administration panel for Debian GNU/Linux
+#    Copyright (C) 2011 Some Hackers In Town
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# -----------------------------------------------------------------------------
+
+import json
+from core.confparser.parser import BaseParser
+
+
+class JSONConfParser (BaseParser):
+    """
+    Generic json type configuration parser.
+    """
+    def __config__(self):
+        if self._buf:
+            self._dict = json.loads(self._buf)
+        else:
+            return None
+
+    def __unicode__(self):
+            return json.dumps(self._dict)
