00f19a56a000d22fe9582fb42cbe474efaeedfa7
Amin Oruji <aminpy@gmail.com>
2011-02-27 19:02:45 +0330

    some bug fixing

--- apps/shop/models.py
+++ apps/shop/models.py
@@ -21,6 +21,7 @@
 
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 
 class Catalog(models.Model):
     name = models.CharField(max_length=255)
