fecf12c76e6152c7dabe0ce7b9938ccc970e6dc4
Sameer Rahmani <lxsameer@gnu.org>
2011-03-19 17:48:00 +0330

    First fork done

--- debbox/core/daemon.py
+++ debbox/core/daemon.py
@@ -19,6 +19,8 @@
 # -----------------------------------------------------------------------------
 
 import os
+import sys
+import atexit
 from ConfigParser import ConfigParser
 
 from debbox.core.servers import WebServer
@@ -30,7 +32,12 @@ class Debbox (object):
     as a deamon.
     """
 
-    def __init__(self, options):
+    def __init__(self, options, stdin='/dev/null', stdout='/dev/null',
+                 stderr='/dev/null'):
+        """
+        Debbox constructor.
+        """
+
         self.options = options
         self.pidfile = options.pidfile
 
@@ -44,6 +51,19 @@ class Debbox (object):
         self.ssl = {"key": self.config.get("SSL", "key"),
                     "cert": self.config.get("SSL", "cert"),
                     }
+        self.stdin = stdin
+        self.stdout = stdout
+        self.stderr = stderr
+
+        # Registering a cleanup method
+        atexit.register(self.__cleanup__)
+
+    def __cleanup__(self):
+        """
+        Debbox destructor.
+        """
+        if os.path.exist(self.pidfile):
+            os.remove(self.pidfile)
 
     def _status(self):
         """
@@ -69,6 +89,38 @@ class Debbox (object):
             print "Debbox is already running."
             return
 
+        # Daemonizing Process ==============================
+        # First Fork
+        pid = None
+        try:
+            pid = os.fork()
+            print ">>>> First fork pid: ", pid
+        except OSError:
+            raise self.CantFork("Can't create the master process.")
+
+        if pid:
+            # Exist from parent
+            sys.exit(0)
+
+        os.umask(027)
+        try:
+            self._sid = os.setsid()
+        except OSError:
+            # TODO: check the exception
+            raise
+
+        # TODO: Where should we chdir? where is the safe place?
+        self.io_redirect()
+
+        # Second Fork =======================================
+        try:
+            self._slavepid = os.fork()
+        except OSError:
+            raise self.CantFork("Can't create the slave process")
+
+        if self._slavepid:
+            # Master Process
+
         server = WebServer(self.options.host, int(self.options.port),
                            self.ssl["key"], self.ssl["cert"],
                            self.options.settings,
@@ -92,6 +144,21 @@ class Debbox (object):
                   (self._masterpid, self._slavepid)
         else:
             print "Debbox is not running."
+
+    def io_redirect(self):
+        if not self.options.debug:
+            # Redirecting standard I/O to nowhere
+            sys.stdout.flush()
+            sys.stderr.flush()
+            si = file(self.stdin, 'r')
+            so = file(self.stdout, 'a+')
+            se = file(self.stderr, 'a+', 0)
+            os.dup2(si.fileno(), sys.stdin.fileno())
+            os.dup2(so.fileno(), sys.stdout.fileno())
+            os.dup2(se.fileno(), sys.stderr.fileno())
+
+    class CantFork (Exception):
+        pass
 
     class CantFindConfigFile (Exception):
         pass
