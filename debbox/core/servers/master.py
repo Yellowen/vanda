c1add9a73300787d64b8fe962e3050129674d1fa
Sameer Rahmani <lxsameer@gnu.org>
2011-03-23 21:09:13 +0430

    comment added on transport protocol

--- debbox/core/servers/master.py
+++ debbox/core/servers/master.py
@@ -17,7 +17,39 @@
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 # -----------------------------------------------------------------------------
 
+import sys
+import pickle
 import json
+
+
+# Transport protocol specification ============================================
+# Request:
+# String format: JSON
+# JSON data structure:
+# {"command":  COMMAND,
+#  "args" : [arg1, arg2, ....],
+# }
+#
+# COMMAND: is the name of a function of method on the Master process
+# args: will be a list of arguments that should be passed to COMMAND function
+#
+# example:
+# {"command": "echo",
+#  "args": ["this is and example",]
+# }
+#
+# Response:
+# String format: JSON
+# JSON data structure:
+# {"status": STATUS,
+#  "message": MSG,
+#  "extra": EXTRA,
+# }
+#
+# STATUS: is a integer value the will be 0 at success and !=0 un failed
+# MSG: is a pickled data that return by Master COMMAND.
+# EXTRA: is a extra flag, each command will use it for its own
+# =============================================================================
 
 
 class Master (object):
@@ -25,12 +57,22 @@ class Master (object):
     Master process application class.
     """
 
-    def __init__(self, logger_instance):
-        self.logger = logger_instance
+    # TODO: make command dictionary loadable form a file
+    commands = {
+        "echo": self.echo,
+        }
 
-    def _dumpmsg(self, status, msg):
+    def __init__(self, logger_instance, debug=False):
+        self.logger = logger_instance
+        self.debug = debug
+
+    def _dumpmsg(self, status, msg, extra=None):
+        """
+        return a json form data message to transport via socket
+        """
         return json.dumps({"status": status,
-                           "message": msg})
+                           "message": msg,
+                           "extra": extra})
 
     def handler(self, socket, address):
         """
@@ -41,7 +83,12 @@ class Master (object):
         """
         # using a makefile because we want to use readline()
         fileobj = socket.makefile()
+
+        # main loop of master process
+        # all the received data will proccess here
         while True:
+
+            # read a line from socket
             line = fileobj.readline()
             if not line:
                 # client disconnected
@@ -57,3 +104,26 @@ class Master (object):
                 fileobj.write(self._dumpmsg(-1, "invalid format"))
                 continue
 
+            if "command" in data:
+                command = data["command"]
+                args = {}
+                if "args" in data:
+                    args = data["args"]
+                if command in self.commands:
+                    try:
+                        result = self.commands[command](**args)
+                    except:
+                        if self.debug:
+                            fileobj.write(self._dumpmsg(-1, pickle.dumps(-1,
+                                                                sys.exc_info(),
+                                                                extra="debug")
+                                                        ))
+                            continue
+                        else:
+                            pass
+                else:
+                    fileobj.write(self._dumpmsg(-1, "command not found"))
+                    continue
+            else:
+                fileobj.write(self._dumpmsg(-1, "malform command"))
+                continue
