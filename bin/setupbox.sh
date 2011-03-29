5bdd2a6b9a6ef120e0d0f9349bb41a51322f7300
Sameer Rahmani <lxsameer@gnu.org>
2011-03-29 21:49:40 +0430

    setup script done

--- bin/setupbox.sh
+++ bin/setupbox.sh
@@ -1,8 +1,10 @@
-#! /bin/sh
+B1;2403;0c#! /bin/sh
 
 # this script should run with root user.
 
-adduser --system --no-create-home debbox debbox
+adduser --system --no-create-home debbox
+addgroup --system debbox
+adduser debbox debbox
 mkdir /var/log/debbox -p
 mkdir /var/lib/debbox -p
 chown debbox.debbox /var/lib/debbox -R
