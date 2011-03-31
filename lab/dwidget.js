46e7054ca5a166ce739c45e1153ebca94115c68f
Sameer Rahmani <lxsameer@gnu.org>
2011-03-31 11:04:50 +0430

    first dwidget schema

--- lab/dwidget.js
+++ lab/dwidget.js
@@ -1,7 +1,47 @@
-(function($) {  
-    $.fn.dwidget = function(params) {
+(function($) {
+    // plugin defination
+    $.fn.dwidget = function(parameters) {
 	
-	var element = this;
+	var self = this;
+
+	var params = parameters || {};
+	var defaults = {width: 100,
+			 height: 100
+			};
+
+	if (window.debug == true)
+	    self.debug = true;
+	else
+	    self.debug = false;
+
+	self.log = function(msg){
+	    if (self.debug == true)
+		console.log("[dwidget] >>> " + msg);
+	};
+	
+	// Setup development environment -------
+	if (self.debug){
+	    $(self).css("background", "#444444");
+	    $(self).css("color", "#eeeeee");
+	}
+
+	// --------------------------------------
+	
+	getparam = function(param){
+	    return params[param] || defaults[param] || undefined;
+	};
+
+	// Setup widget --------------------------
+	self.log(getparam("width"));
+	$(self).css("position", "absolute");
+	$(self).css("width", getparam("width"));
+	$(self).css("height", getparam("height"));
+	
+	self.create_child = function(params){};
+
+
+	$(self).html("Hello world " + self.debug);
+	return self;
 
 };  
 })(jQuery); 
