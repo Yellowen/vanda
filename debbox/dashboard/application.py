85287477de7f7fa2fdccb4c0a0b359330c3a06ec
Sameer Rahmani <lxsameer@gnu.org>
2011-05-15 09:49:09 +0430

    dashboard application interface bug fixed

--- debbox/dashboard/application.py
+++ debbox/dashboard/application.py
@@ -17,7 +17,7 @@
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 # -----------------------------------------------------------------------------
 
-from debbox.core.vpkg.interface import BaseApplication
+from vpkg.interface import BaseApplication
 
 
 class Dashboard (BaseApplication):
