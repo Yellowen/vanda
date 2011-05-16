407389c27b3cfb066a86573b5f2592acf9e365e7
Sameer Rahmani <lxsameer@gnu.org>
2011-05-16 12:35:35 +0430

    registering item moved to section node class

--- debbox/dashboard/base.py
+++ debbox/dashboard/base.py
@@ -26,6 +26,35 @@ class SectionNode(object):
     title = None
     name = None
 
+    def __init__(self):
+        self._item_registry = {}
+
+    def register_item(self, item_node):
+        """
+        Registering a item_node.
+        """
+        # item_class should be direct or indirect instance of ItemNode
+        # class
+        if not issubclass(item_node, ItemNode):
+            raise TypeError("'%s' shoud be a subclass of ItemNode" %
+                            item_node.__class__)
+        # Checking the section name.
+        try:
+            # each itemNode subclass should have a name property
+            item_name = item_node.__getattr__("name")
+        except AttributeError:
+            raise AttributeError("'%s' node does not have a " %
+                                 item_node.__class__ +
+                                 "'name' property")
+        if item_name in self._item_registry.keys():
+            # TODO: Can new item class override the exists one?
+            self.logger.debug("An Item with the '%s' name already registered.")
+            return False
+
+        # registring item_node
+        self._registry[item_name] = item_node()
+        return True
+
 
 class ItemNode(object):
     """
