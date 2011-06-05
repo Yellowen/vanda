8c0cc5fb049072f9e4534261ae51fa7453f019e5
Behnam Ahmad Khan Beigi <b3hnam@gnu.org>
2011-06-05 16:39:02 +0430

    Blog urls edited

--- urls.py
+++ urls.py
@@ -1,17 +1,28 @@
-from django.conf.urls.defaults import patterns, include, url
+# ---------------------------------------------------------------------------
+#    Vanda Project
+#    Copyright (C) 2010  Dina Project Community
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# ---------------------------------------------------------------------------
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+
+from django.conf.urls.defaults import patterns
+import views
+
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ult.views.home', name='home'),
-    # url(r'^ult/', include('ult.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    (r'^$', views.blog  ),
+    (r'^post/(.*)/$', views.comments  ),
 )
