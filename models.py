3713db4fb5a284ac59b9c707a7d5457416e6a8a2
Sameer Rahmani <lxsameer@gnu.org>
2011-10-23 15:49:03 +0330

    Refactoring

--- models.py
+++ models.py
@@ -21,35 +21,29 @@ from django.db import models
 from django.contrib.admin.models import User
 from django.utils.translation import ugettext as _
 from django.template import Template, Context
-#from dina import conf
 
 
 class Category(models.Model):
     """
-    each post will be tagged for just one category.
+    Each post will be tagged for just one category.
     """
 
-    title = models.CharField(max_length=250,\
+    title = models.CharField(max_length=250,
                              unique=True, verbose_name=_("Title"))
-    slug = models.SlugField(max_length=100,\
-                            verbose_name=_("Slug"),\
+    slug = models.SlugField(max_length=100,
+                            verbose_name=_("Slug"),
                             help_text=_("This Field will fill\
                             automaticaly by title"))
-    
-    #Icon = models.ImageField(upload_to="//",verbose_name=_("Description"))
-#    CATS = (
-#        ('text', _('Text')),
-#        ('link', _('Link')),
-#        ('music', _('Music')),
-#        ('book', _('Book')),
-#        ('film', _('Film')),
-#        ('person', _('Person or band')),
-#    )
-    category_type = models.CharField(max_length=250, \
-                                      verbose_name=("Type"),)
+
+    parent = models.ForeignKey('self', verbose_name=_("Parent"),
+                               blank=True, null=True)
 
     def __unicode__(self):
         return self.title
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('ultra_blog.views.category_view', self.slug)
 
     class Meta:
         verbose_name_plural = _("Categories")
@@ -60,17 +54,15 @@ class Category(models.Model):
 class Post (models.Model):
     """
     Post model.
-    Notes:
-          Use get_content() instead of content
-          author and datetime will be filled automaticly.
+    author and datetime will be filled automaticly.
     """
     title = models.CharField(max_length=250, verbose_name=_("Title"))
     slug = models.SlugField(max_length=100, verbose_name=_("Slug"),\
                             unique=True,\
                             help_text=_("This field will fill automaticly \
                             by title field."))
-    body = models.TextField(verbose_name=_("Content"))
-    categoriey = models.ForeignKey(Category, verbose_name=_("Categories"))
+    content = models.TextField(verbose_name=_("Content"))
+    categories = models.ManyToManyField(Category, verbose_name=_("Categories"))
     author = models.ForeignKey(User, verbose_name=_("Author"))
     datetime = models.DateTimeField(auto_now_add=True, editable=False,\
                                      verbose_name=_('Date and Time'))
@@ -79,26 +71,13 @@ class Post (models.Model):
         """
         Return suitable content by looking up settings.
         """
-        setting = Setting.configs()
-        maxbl = 400
-        if setting.max_body_length:
-            maxbl = setting.max_body_length
-        # TODO: add ... to end of content
-        return self.content()
-        #return Template("%s<p> ...</p>" % self.body[:maxbl]
-        #                          ).render(Context())
-
-    def content(self):
-        """
-        return HTMLize body content.
-        """
-        return Template(self.body).render(Context())
+        return self.content
 
     def comments(self):
         """
         Return the comments related to current post.
         """
-        return Comment.objects.filter(post=self).order_by('-datetime')
+        return Comment.objects.filter(post=self)
 
     def related_posts(self):
         return Post.objects.filter(categories__in=self.categories.all())[5]
@@ -106,10 +85,12 @@ class Post (models.Model):
     def __unicode__(self):
         return self.title
 
+    @models.permalink
     def get_absolute_url(self):
-        return "/blog/post/%s" % self.slug
+        return ("ultra_blog.views.post_view", self.slug)
 
     class Meta:
+        ordering = ["-datetime"]
         verbose_name_plural = _("Posts")
         verbose_name = _('Post')
 
@@ -117,69 +98,83 @@ class Post (models.Model):
 class Comment (models.Model):
     """
     Comment model.
-    Notes:
-           Author of a comment may be empty if 'allow_anonymous' config set
-           to True. In this case nick will hold the nickname that user provided
+    author of a comment may be empty if user is anonymous.
+    In this case nick will hold the nickname that user provided
 
-           But if author filled with current user->username then nick fill with
-           the same value.
+    But if author filled with current user->username then nick fill with
+    the same value.
     """
 
     post = models.ForeignKey(Post, verbose_name=_("Post"))
-    author = models.ForeignKey(User, verbose_name=_("Author"), blank=True,\
+    author = models.ForeignKey(User, verbose_name=_("Author"),
+                               blank=True,
                                 null=True)
+
     nick = models.CharField(max_length=40,
-                            verbose_name=_("Nickname"), blank=True,\
+                            verbose_name=_("Nickname"),
+                            blank=True,
                             null=True)
+
     email = models.EmailField(blank=True, null=True,
                               verbose_name=_("Email (Optional)"))
+
     content = models.TextField(verbose_name=_("Your Comment"))
-    datetime = models.DateTimeField(auto_now_add=True, editable=False,\
+    datetime = models.DateTimeField(auto_now_add=True, editable=False,
                                      verbose_name=_('Date and Time'))
 
     def __unicode__(self):
-        return "Comment on %s - %s" % (
-            self.post.title, "%s..." % self.content[:30])
-
-    def get_absolute_url(self):
-        return "/blog/comments/%s" % self.id
+        return "Comment on %s - %s" % (self.post.title,
+                                       "%s..." % self.content[:30])
 
     class Meta:
         verbose_name_plural = _("Comments")
         verbose_name = _('Comment')
+        ordering = ["-datetime"]
 
 
-class Setting (conf.Config):
+class Setting (models.Model):
     """
     Configuration model.
     """
-    allow_anonymous_comment=conf.BooleanField(default=True,\
-                        verbose_name=_("Allow anonymous comments?"),\
-                        help_text=_("Allow to un-registered user to\
-                        comment your posts."))
-    post_per_page = conf.IntegerField(default=10,\
+    _DEFAULT = {
+        'post_per_page': 10,
+        'comment_per_page': 10,
+        }
+    active = models.BooleanField(_("Active"),
+                                 default=False)
+
+    post_per_page = models.IntegerField(default=10,
                             verbose_name=_("How many post per page?"))
-    comment_per_page = conf.IntegerField(default=10,\
+
+    comment_per_page = models.IntegerField(default=10,
                             verbose_name=_("How many comment per page?"))
-    max_body_length = conf.IntegerField(default=400,\
-                            verbose_name=_("Maximume content"),\
-                            help_text=_("Maximume character in content"))
-    categories_count = conf.IntegerField(default=20,\
-                            verbose_name=_("Categories count"),\
-                            help_text=_("The number of categories shown in\
-                            side bar."))
+
+    @staticmethod
+    def get_setting(setting_name, default=None):
+        """
+        Return the field data of given setting_name if exists or
+        return default value for it.
+        """
+        if setting_name in _DEFAULT:
+            try:
+                return getattr(Setting.objects.get(active=True), setting_name)
+            except Setting.DoesNotExist:
+                return _DEFAULT[setting_name]
+        else:
+            return default
+
+    def save(self, *argc, **kwargs):
+        """
+        Only one active setting allowed.
+        """
+        if self.active:
+            try:
+                pre = Setting.objects.get(active=True)
+            except Setting.DoesNotExist:
+                pass
+            pre.save()
+        super(Setting, self).save(*argc, **kwargs)
 
     class Meta:
         verbose_name_plural = _("Blog Settings")
         verbose_name = _('Setting')
-
-    class ConfigAdmin:
-        fieldsets = (
-        (None, {
-            'fields': (('allow_anonymous_comment',\
-                        'max_body_length'),\
-                       ('post_per_page',\
-                        'comment_per_page',\
-                        'categories_count'))
-        }),
-    )
