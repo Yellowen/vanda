1e7a66e7060e6064fc2d7cddc7eecb1e0d1ec5b5
Sameer Rahmani <lxsameer@gnu.org>
2011-12-06 00:18:34 +0330

    unix server added

--- lab/server.js
+++ lab/server.js
@@ -1,4 +1,22 @@
 var io = require('socket.io').listen(9000);
+var net = require('net');
+
+var unix = net.createServer(function(c) {
+    console.log('Unix server connected');
+    c.on('end', function() {
+	console.log('Unix server disconnected');
+    });
+    
+    c.on('data', function(data){
+	console.log('[RECV]: ' + data);
+
+    });
+});
+
+unix.listen("/tmp/websucks.sock", function() {
+    console.log("unix listen");
+});
+
 
 io.configure(function () {
   io.set('transports', ['websocket']);
