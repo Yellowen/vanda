2136d0fe4cf43c5264012b3422a590792119b450
Behnam Ahmad Khan Beigi <b3hnam@gnu.org>
2011-12-15 16:21:45 +0330

    categories template tag changed

---
+++ templatetags/categories_list.py
@@ -0,0 +1,35 @@
+from django import template
+
+from ultra_blog.models.base import Category
+
+
+register = template.Library()
+
+
+def categories_list(parser, token):
+    return CategoriesNode()
+
+
+class CategoriesNode(template.Node):
+
+    def render(self, context):
+        return self.get_categories()
+
+    def get_categories(self, cat=None):
+        """
+        Get categories
+        """
+        cats = Category.objects.filter(parent=cat)
+        result = ""
+        for cat in cats:
+            result += """
+            <ul>
+            <li><a href="/blog/filter/?category=%s">%s</a></li>    
+            """ % (cat.title, cat.title)
+            result += self.get_categories(cat)
+            result += """
+            </ul>
+            """
+        return template.Template(result).render(template.Context())
+
+register.tag('categorieslist', categories_list)
