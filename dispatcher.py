563099d5055553aae855ec52c86219a2c69264d0
Sameer Rahmani <lxsameer@gnu.org>
2012-05-09 21:10:53 +0430

    cookie set code added

--- dispatcher.py
+++ dispatcher.py
@@ -16,21 +16,56 @@
 #    with this program; if not, write to the Free Software Foundation, Inc.,
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 # -----------------------------------------------------------------------------
+import datetime
 
 from django.core.urlresolvers import resolve
 from django.conf import settings
 from django.http import Http404
 
 
+def set_cookie(response, key, value, days_expire=7):
+    if days_expire is None:
+        max_age = 365 * 24 * 60 * 60  # One year
+    else:
+        max_age = days_expire * 24 * 60 * 60
+    expires = datetime.datetime.strftime(
+        datetime.datetime.utcnow() + datetime.timedelta(seconds=max_age),
+        "%a, %d-%b-%Y %H:%M:%S GMT")
+    response.set_cookie(key,
+                        value,
+                        max_age=max_age,
+                        expires=expires,
+                        domain=settings.SESSION_COOKIE_DOMAIN,
+                        secure=settings.SESSION_COOKIE_SECURE or None)
+
+
 def dispatch_url(request, lang=None):
     """
     Dispatch the urls again against the LEAF_URLCONF.
     """
+    _lang = lang
+    need_cookie = True
 
     if lang:
         request.path = request.path[len(lang) + 1:]
+
+    else:
+        if "language" in request.COOKIES:
+            _lang = request.COOKIES["language"]
+            need_cookie = False
+        else:
+            if "language" in request.session:
+                _lang = request.session["language"]
+            else:
+                _lang = settings.LANGUAGES[0][0]
+
     try:
         view = resolve(request.path, settings.LEAF_URLCONF)
-        return view.func(request, *view.args, **view.kwargs)
+        response = view.func(request, *view.args, **view.kwargs)
+        if need_cookie:
+            set_cookie(response, "language", _lang)
+
+        return response
+
     except Http404:
         raise
