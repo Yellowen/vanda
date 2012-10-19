6fce00054f93f01613a46873fb4030548351189e
Sameer Rahmani <lxsameer@gnu.org>
2012-10-19 14:36:24 +0330

    Restructing source tree

--- vanda/ultra_blog/dashboard/tables/__init__.py
+++
@@ -1,4 +0,0 @@
-from post import posts
-from comment import comments
-from category import categories
-
--- vanda/ultra_blog/statics/js/js_init.js
+++
@@ -1,4 +0,0 @@
-function addpanel () {
-    var area2 = new nicEditor({fullPanel : true}).panelInstance('id_content');
-}
-bkLib.onDomLoaded(function() { addpanel(); });
