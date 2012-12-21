58c5918e124fa82f9249c0edea8dc13bdf8aa1cf
Sameer Rahmani <lxsameer@gnu.org>
2012-12-21 22:24:39 +0330

    adding widgets done

--- vanda/apps/dashboard/statics/js/blocks/widgetarea.js
+++ vanda/apps/dashboard/statics/js/blocks/widgetarea.js
@@ -3,10 +3,13 @@ function WidgetArea (name, es) {
 
     var el = this.element_id;
     var self = this;
-    /*$(el).ready(function (){
-	
-    };*/
-
+    $(el).ready(function (){
+	$(".closebtn").live("click", function(){
+	    var widgetname = $(this).parent().attr("widgetname");
+	    $("#id_" + widgetname + "_widget").fadeOut(600, function(){$(this).remove()});
+	    delete document.dashboard.widgets[widgetname];
+	});
+    });
 };
 
 WidgetArea.prototype = new Block();
@@ -28,21 +31,29 @@ WidgetArea.prototype.append_widget = function (widget) {
 		block = document.tmp;
 		delete document.tmp;
 		block.widgets[widget.name] = widget;
-		new_widget = $("#widget_data").html();
-		$(new_widget).filter(".widgetbox").attr("id", "id_" + widget.name + "_wbox");
-		console.log($(new_widget).filter(".widgetbar"));
-		$(new_widget).filter(".widget").last().attr("id", "id_" + widget.name + "_wbar");
-		console.log(new_widget);
-		$(new_widget).filter(".widgetbox").html(data);
-		$(block.es).append(new_widget);
-		var width =  $(block.es).find("#id_" + widget.name + "_wbox").css("width");
-		var p = $(block.es).find("#id_" + widget.name + "_wbox").css("padding-top");
-		var w = $(block.es).find("#id_" + widget.name + "_wbar").width();
-		console.log(w);
+		var new_widget = $("#widget_data").html();
+		$("#tmp_widget").html(new_widget);
+		new_widget = $("#tmp_widget");
+		$(new_widget).find(".widgetbox").attr("id", "id_" + widget.name + "_wbox");
+		$(new_widget).find(".widgetbox").attr("widgetname", widget.name);
 
-		$(block.es).find(".widget").css("width", aa + 58);
-		$(block.es).find(".widget").attr("id", "id_" + widget.name + "_widget");
-		
+		$(new_widget).find(".widgetbar").attr("id", "id_" + widget.name + "_wbar");
+		$(new_widget).find(".widgetbar").attr("widgetname", widget.name);
+
+		$(new_widget).find(".widget").attr("id", "id_" + widget.name + "_widget");
+		$(new_widget).find(".widget").hide();
+		$(new_widget).find(".widgetbox").html(data);
+		var p = parseInt($(new_widget).find("#id_" + widget.name + "_wbox").css("padding-top"));
+		var w = $(new_widget).find("#id_" + widget.name + "_wbar").width();
+
+		$(block.es).append(new_widget.html());
+		$(block.es).find("#id_" + widget.name + "_widget").fadeIn(600);
+		var width =  parseInt($(block.es).find("#id_" + widget.name + "_wbox").width());
+		var fwidth = width + p * 2 + w;
+
+		$(block.es).find("#id_"+ widget.name + "_widget").css("width", fwidth);
+
+		$("#tmp_widget").html("");
 	    }
 	   });
 };
