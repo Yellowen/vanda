72f148077393bbf71ea078160a688d1d0a44e20e
Sameer Rahmani <lxsameer@gnu.org>
2013-03-20 00:18:17 +0330

    less application added

---
+++ vanda/js/less/static/vanda/js/less/less_init.js
@@ -0,0 +1,12 @@
+var ctag = document.getElementById("less_init");
+less = {
+    env: ctag.dataset.env || "development",
+    async: ctag.dataset.async || false,
+    fileAsync: false,
+    poll: 1000,
+    functions: {},
+    dumpLineNumbers: "comments",
+    relativeUrls: false,
+    rootpath: ctag.dataset.rootpath || "/static/"
+};
+console.log(less);
